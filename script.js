b999b35bcf6cbacf56e416d055fc14486d5f33af
Formboy60 <formboy@gmail.com>
2022-09-03 16:44:32 +0200

    commit

--- script.js
+++ script.js
@@ -1,89 +1,50 @@
-const haut = document.querySelector('.haut');
-const mid = document.querySelector('.mid');
-const start = document.getElementById('strt');
-const stop = document.getElementById('stp');
-const reset = document.getElementById('rst');
-const start1 = document.getElementById('strt1');
-const stop1 = document.getElementById('stp1');
-const reset1 = document.getElementById('rst1');
-let sec = 0;
-let min = 0;
-let hrs = 0;
-let t;
-let sec1 = 0;
-let min1 = 0;
-let hrs1 = 0;
-let t1;
+var timeStart = 0;
+var timeEnd = 0
+let stock = 0
 
 
 
-// premier timer
-
-function tick1(){
-    sec++;
-    if (sec >= 60) {
-        sec = 0;
-        min++;
-        if (min >= 60) {
-            min = 0;
-            hrs++;
-        }
-    }
-}
-function add1() {
-    tick1();
-    haut.textContent = (hrs > 9 ? hrs : "0" + hrs) 
-        	 + ":" + (min > 9 ? min : "0" + min)
-       		 + ":" + (sec > 9 ? sec : "0" + sec);
-    timer1();
-}
-function timer1() {
-    t = setTimeout(add1, 1000);
-}
-
-start.onclick = timer1;
-stop.onclick = function() {
-    clearTimeout(t);
-}
-reset.onclick = function() {
-    haut.textContent = "00:00:00";
-    sec = 0; min = 0; hrs = 0;
-}
 
 
+let p = document.querySelector('p')
+let str = document.querySelector('.srt')
+let stp = document.querySelector('.stp')
 
-// deuxieme timer
+str.addEventListener('click', () => {
+  if(timeStart > 0 ){
+    return
+  }
+  timeStart = new Date(Date.now()).getTime()
+  p.style.color = "green"
+  console.log(timeStart);
+})
 
-function tick2(){
-    sec1++;
-    if (sec1 >= 60) {
-        sec1 = 0;
-        min1++;
-        if (min1 >= 60) {
-            min1 = 0;
-            hrs1++;
-        }
-    }
-}
-function add2() {
-    tick2();
-    mid.textContent = (hrs1 > 9 ? hrs1 : "0" + hrs1) 
-        	 + ":" + (min1 > 9 ? min1 : "0" + min1)
-       		 + ":" + (sec1 > 9 ? sec1 : "0" + sec1);
-    timer2();
-}
-function timer2() {
-    t1 = setTimeout(add2, 1000);
-}
+stp.addEventListener('click', () => {
 
-start1.onclick = timer2;
-stop1.onclick = function() {
-    clearTimeout(t1);
-}
-reset1.onclick = function() {
-    mid.textContent = "00:00:00";
-    sec1 = 0; min1 = 0; hrs1 = 0;
-}
+  if(timeStart == 0){
+    return
+  } 
+
+  timeEnd = new Date(Date.now()).getTime()
+  let hourDiff = timeEnd - timeStart;
+   
+  if(stock == 0){
+  stock = hourDiff
+  } else { stock = stock + hourDiff}
+
+  console.log(stock);
+  timeStart = 0
+  
+  var secDiff = stock / 1000; //in s
+  var minDiff = stock / 60 / 1000; //in minutes
+  var hDiff = stock / 3600 / 1000; //in hours
+  var humanReadable = {};
+  humanReadable.hours = Math.floor(hDiff);
+  humanReadable.minutes = Math.floor(minDiff - 60 * humanReadable.hours);
+  
+  p.innerHTML= humanReadable.hours + 'h : ' + humanReadable.minutes +'min'
+  p.style.color = "red"
+})
 
 // implémentation tableau
 
@@ -111,23 +72,13 @@ document.querySelector('.push').addEventListener('click', () => {
     localStorage.setItem('tab', document.querySelector('.tableau').innerHTML)
     document.querySelector(".dossier1").value = ''
     document.querySelector(".tache1").value =''
-    haut.textContent = "00:00:00"
+    p.textContent = "0h : 0min"
+    p.style.color = 'black'
     sec = 0; min = 0; hrs = 0;
     console.log(sec);
 })
 
-document.querySelector('.push1').addEventListener('click', () => {    
-    let dossier = document.querySelector(".dossier2").value
-    let tache = document.querySelector(".tache2").value
-    let temps = document.querySelector('.mid').textContent
-    ajoutLigne(dossier, tache, temps)
-    localStorage.setItem('tab', document.querySelector('.tableau').innerHTML)
-    document.querySelector(".dossier2").value = ''
-    document.querySelector(".tache2").value =''
-    mid.textContent = "00:00:00"
-    sec1 = 0; min1 = 0; hrs1 = 0;
-    console.log(sec1);
-})
+
 
 document.querySelector('.effTab').addEventListener('click', () => {
     localStorage.clear()
